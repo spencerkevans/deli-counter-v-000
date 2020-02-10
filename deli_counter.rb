02a28a2282521b58c69096ace8a07112d8385ebb
spensation <spencevans@gmail.com>
2020-02-09 20:39:56 -0500

    #line is working

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def  line(deli)
+	in_line = ""
+	if (deli.length < 1)
+		puts "The line is currently empty."
+	elsif (deli.length > 0)
+		
+		deli.to_enum.with_index(1).each do |elem, i|
+			in_line += "#{i}. #{elem} "
+		end
+		puts "The line is currently: " + in_line[0..-2]
+	end
+end
